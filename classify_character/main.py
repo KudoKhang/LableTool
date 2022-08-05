8d117429114764b3f7b9f1721cd245e0ef6dfa06
KudoKhang <thanhchem.k39a1@gmail.com>
2022-08-05 09:29:56 +0700

    add functions show original image

--- classify_character/main.py
+++ classify_character/main.py
@@ -8,15 +8,16 @@ import argparse
 
 def get_arguments():
     arg = argparse.ArgumentParser()
-    arg.add_argument('-in', '--input_f', help='link to image', default='image/')
-    arg.add_argument('-out', '--output_f', help='link to save label image', default='output/')
+    arg.add_argument('-char', '--character', help='link to character folder', default='chars/')
+    arg.add_argument('-img', '--image', help='link to image folder', default='images/')
+    arg.add_argument('-out', '--output_f', help='link folder to save annotations ', default='output/')
     arg.add_argument('-i', '--index', help='index image in folder input_f', default=0)
     return arg.parse_args()
 
 args = get_arguments()
 
 # input_f = 'image/'
-input_path = [args.input_f + name for name in os.listdir(args.input_f) if name.endswith(('jpg', 'png', 'jpeg'))]
+input_path = [args.character + name for name in os.listdir(args.character) if name.endswith(('jpg', 'png', 'jpeg'))]
 
 i = args.index
 k = 0
@@ -35,29 +36,39 @@ def create_folder_output(output_f='output/'):
 
 create_folder_output(args.output_f)
 
-img = cv2.imread(input_path[i])
+def readImage(input_path, i):
+    global img
+    char = cv2.imread(input_path[i])
+    id = input_path[i].split('/')[-1].split('_')[0]
+    img = cv2.imread(f"{args.image}/{id}.jpg")
+    h, w = char.shape[:2]
+    blend_plape = cv2.addWeighted(char, 0.6, img[:h, :w], 0.4, 0)
+    img[:h, :w] = blend_plape
+
+readImage(input_path, i)
 
 print('--------------------TUTORIAL-------------------------')
-print('HOW TO USGE: Press character or digital corresponding with image to annotation data')
-print('Next: press [.] to next image')
-print('Previous: press [,] to previous image')
-print('Quit: press [Q] to quit')
-print(f'Start annotation at index image [{i}]')
-print(f'Labels have save in {args.output_f} folder')
+print('HOW TO USGE: Press character or digital corresponding with image to annotation data, '
+      'Press / with character undefined.')
+print('Next: press [.] to next image.')
+print('Previous: press [,] to previous image.')
+print('Quit: press [Q] to quit (shift + q).')
+print(f'Start annotation at index image [{i}].')
+print(f'Labels have save in {args.output_f} folder.')
 print('-----------------------------------------------------')
 
 while k != ord('Q'):
-    cv2.imshow("Classify character", img)
+    cv2.imshow("Classify Character - Dev by K2", img)
     k = cv2.waitKey(20) & 0xFF
 
     # Next or Previos
     if k == ord('.'):
         i = 0 if i == len(input_path) - 1 else i + 1
-        img = cv2.imread(input_path[i])
+        readImage(input_path, i)
+
     if k == ord(','):
         i = len(input_path) - 1 if i == -1 else i - 1
-        img = cv2.imread(input_path[i])
-
+        readImage(input_path, i)
     # Digital & character
     for c in l_char:
         if k == ord(c):
@@ -65,6 +76,6 @@ while k != ord('Q'):
                 c = 'Background'
             shutil.copy(input_path[i], os.path.join(args.output_f, c))
             i = 0 if i == len(input_path) - 1 else i + 1
-            img = cv2.imread(input_path[i])
+            readImage(input_path, i)
             print(f'Index image: [{i}] - Copy {input_path[i]} to folder label {c.capitalize()}')
 print(f'Break at index image [{i}]')
